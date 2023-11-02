999fb7a6847ae346c95222ad09efaa4936409055
Mrinal Deb <mrinaldeb@Mrinals-MacBook-Air.local>
2023-11-03 01:20:23 +0530

    firebase setup hogaya

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,37 @@
-import data from './Data.json'
+// import data from './Data.json'
 import Card from './Card'
-import { useState } from 'react'
-
-const [loading,setLoading] =useState(true);
+import { useEffect, useState } from 'react'
+import {  MagnifyingGlass } from 'react-loader-spinner';
+import { getDocs } from 'firebase/firestore';
+import { moviesRef } from '../Firebase/Firebase';
 //say hi
 
+
 const Cards = () => {
+
+  const [data,setData] = useState([]);
+  const [loading,setLoading] =useState(true);
+  useEffect(()=>{
+    async function getData(){
+      const _data= await getDocs(moviesRef);
+      console.log(_data);
+      _data.forEach((doc)=>{
+        setData((prev)=>[...prev,doc.data()]);
+      })
+    }
+    setLoading(true);
+    getData();
+    setLoading(false);
+  },[])
+
   return (
-    
-    <div className='flex flex-wrap justify-between pt-3 pl-5 pr-5 mt-5'>
-    {
+    <div className='flex flex-wrap pt-3 pl-5 pr-5 mt-5'>
+    {loading?<div className='flex justify-center items-center w-full h-96'> <MagnifyingGlass height={80}  color='white' /></div> :
       data.map((e,i)=>{
         return <Card e={e} i={i} />
       })
+    
     }
-
-
     </div>
   );
 };
